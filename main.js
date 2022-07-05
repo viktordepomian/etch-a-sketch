3f5dda1301ee3b215c4eed8fd2bda9f98aeff13b
viktordepomian <viktor.depomian@gmail.com>
2022-07-05 12:24:19 +0200

    Changes to js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const container = document.querySelector('.container');
+const squaresInput = prompt('Please input how many squares(e.g. 16, 32, 64) Max: 100.');
 
-
-function createSquareDivs(){
-    const newSquare = document.createElement('div');
-    newSquare.classList.add('squareStyle');
-    container.appendChild(newSquare);
+function makeRowsYo(rows, cols){
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for(x = 0; x < (rows * cols); x++){
+        let newSquare = document.createElement('div');
+        container.appendChild(newSquare);
+        newSquare.classList.add('squareStyle');
+        newSquare.addEventListener('click', () => {
+            makeSquareColored();
+        });
+        function makeSquareColored(){
+            newSquare.classList.toggle('coloredSquare')   ;
+        }
+    };
 };
 
-createSquareDivs();
+
+makeRowsYo(squaresInput, squaresInput);
